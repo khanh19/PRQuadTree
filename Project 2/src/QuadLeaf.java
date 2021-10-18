2f646d83ac2aaeb5a61dbfe50111740e7a3cdaa3
Cuong Pham <cpham006@vt.edu>
2021-10-17 23:17:55 -0400

    QuadInternal and QuadLeaf

---
+++ Project 2/src/QuadLeaf.java
@@ -0,0 +1,16 @@
+
+public class QuadLeaf<E> {
+    private E element;
+    
+    QuadLeaf(E theElement){
+        element = theElement;
+    }
+    
+    public E getElement() {
+        return element;
+    }
+    
+    public void setElement(E value) {
+        element = value;
+    }
+}
